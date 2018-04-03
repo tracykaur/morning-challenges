4d1a47b2fcd79bf9de7c414fcd24a850ff6cf443
tracykaur <tracykaur@yahoo.com>
2018-04-04 09:45:16 +1000

    income_tax challenge

---
+++ 18_income_tax.rb
@@ -0,0 +1,19 @@
+#   Here are the Australian 2017–18 tax rates.
+#
+#   INCOME              TAX ON INCOME
+#   0 – $18,200         Nil
+#   $18,201 – $37,000   19c for each $1 over $18,200
+#   $37,001 – $87,000   $3,572 plus 32.5c for each $1 over $37,000
+#   $87,001 – $180,000  $19,822 plus 37c for each $1 over $87,000
+#   $180,001 and over   $54,232 plus 45c for each $1 over $180,000
+#
+#   Complete the calculate_tax method, it should return a
+#   hash containing: income, tax, income_after_tax
+#
+#   Test your code:
+#   ruby tests/18_income_tax_test.rb
+#
+
+def calculate_income_tax (income)
+  # Your code here!
+end
