80f805338714f74101c0fbf78da38ccaca7977b8
tracykaur <tracykaur@yahoo.com>
2018-04-17 08:27:32 +1000

    phones challenge

---
+++ 20_packing_phones.rb
@@ -0,0 +1,2 @@
+
+# 2. A business called “The Company” created a new phone called the companyPhone that needs packing to send away for sale. The Company needs to know how many phones in their pretty new packaging (rectangular prisms) that measures: with  width 5cm, length 7.4cm, and depth 4cm we can fit into a box that is 32cm wide,  43cm long and 22.1cm high.   (think about writing some pseudo code first that steps through all the commands you need to write. Keep it as simple as you can.
