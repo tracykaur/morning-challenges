0ab72a41d2ecc7aebbb3ad3794d1cb5aeb90acd2
tracykaur <tracykaur@yahoo.com>
2018-04-24 08:56:00 +1000

    longest word challenge

---
+++ 25_longest_sweet_word.rb
@@ -0,0 +1,8 @@
+# Return the length of the longest word in the provided sentence.
+# Your response should be a number.
+
+# def findLongestWord(str) {
+
+# }
+
+# findLongestWord("Cupcake ipsum dolor sit amet powder fruitcake carrot cake sweet. Candy carrot cake brownie danish consectetur macaroon wafer cake dragée. Lemon drops jelly beans gummi bears topping muffin pastry. Chupa chups muffin tart exercitation cotton candy. Cake gingerbread I love candy canes chocolate bar I love. Danish dessert bonbon muffin chupa chups. Ice cream fruitcake chupa chups pastry wafer carrot cake apple pie. I love I love reprehenderit cotton candy brownie I love jelly. Cookie jelly-o caramels candy. Carrot cake topping cheesecake donut.");
