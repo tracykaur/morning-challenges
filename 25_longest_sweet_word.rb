c5d92504e3e0d5852b1e964fa93164aa60df9b48
Jeff Wong <jeffmjwong@gmail.com>
2018-04-24 11:11:36 +1000

    Solve longest-sweet-word

--- 25_longest_sweet_word.rb
+++ 25_longest_sweet_word.rb
@@ -1,8 +1,23 @@
 # Return the length of the longest word in the provided sentence.
 # Your response should be a number.
 
-# def findLongestWord(str) {
+str = "Cupcake ipsum dolor sit amet powder fruitcake carrot cake sweet.
+        Candy carrot cake brownie danish consectetur macaroon wafer cake
+        dragée. Lemon drops jelly beans gummi bears topping muffin pastry.
+        Chupa chups muffin tart exercitation cotton candy. Cake gingerbread
+        I love candy canes chocolate bar I love. Danish dessert bonbon
+        muffin chupa chups. Ice cream fruitcake chupa chups pastry wafer
+        carrot cake apple pie. I love I love reprehenderit cotton candy
+        brownie I love jelly. Cookie jelly-o caramels candy. Carrot cake
+        topping cheesecake donut."
 
-# }
+def findLongestWord(str)
+  str_array = str.scan(/[^\.\s]+/)
+  longest_length = 0
+  str_array.each do |word|
+    longest_length = word.length if word.length > longest_length
+  end
+  return longest_length
+end
 
-# findLongestWord("Cupcake ipsum dolor sit amet powder fruitcake carrot cake sweet. Candy carrot cake brownie danish consectetur macaroon wafer cake dragée. Lemon drops jelly beans gummi bears topping muffin pastry. Chupa chups muffin tart exercitation cotton candy. Cake gingerbread I love candy canes chocolate bar I love. Danish dessert bonbon muffin chupa chups. Ice cream fruitcake chupa chups pastry wafer carrot cake apple pie. I love I love reprehenderit cotton candy brownie I love jelly. Cookie jelly-o caramels candy. Carrot cake topping cheesecake donut.");
+puts findLongestWord(str)
