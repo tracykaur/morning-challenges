55bf86a4ed6a51d94a4ff134727a819cf1ee7484
Revel Roestorf <revel.roestorf@gmail.com>
2018-04-26 09:23:08 +1000

    x

--- 24_ruby_fight.rb
+++ 24_ruby_fight.rb
@@ -1,9 +1,51 @@
-# The Company has two directors who are passionately disagreeing, they want to fight it out. Being the fantastic human you are, you propose creating a virtual fight to minimise physical harm.
-# You need to create a simple fighting game, where two opponents will fight until one of them loses.
+# The Company has two directors who are passionately disagreeing, they want to
+# fight it out. Being the fantastic human you are, you propose creating a virtual
+ # fight to minimise physical harm.
+# You need to create a simple fighting game, where two opponents will fight until
+# one of them loses.
 
-# Hints: start by creating a Player class (the instance variables could be name, health and power).
-# You’ll need a few methods, consider one that keeps track of whether a player is alive, another one that calculates the impact of hits (decreases the opponents heath by the the amount of the other players power). And a third that outputs the players information.
+# Hints: start by creating a Player class (the instance variables could be name,
+# health and power).
+# You’ll need a few methods, consider one that keeps track of whether a player is
+# alive, another one that calculates the impact of hits (decreases the opponents
+# heath by the the amount of the other players power). And a third that outputs
+# the players information
 
-# You’ll then need to make a fight method that uses a loop to make the players hit one another until  the is alive method returns false for one of them. When the loop is finished (which would mean one of the players has lost), show the players output.
+# You’ll then need to make a fight method that uses a loop to make the players
+# hit one another until  the is alive method returns false for one of them. When
+# the loop is finished (which would mean one of the players has lost), show the
+# players output.
 
-# Then create two Player objects (create a random value for each player’s health and power, make is so that the maximum health value for a player is 200, and the maximum power value is 25) and call the fight method.
+# Then create two Player objects (create a random value for each player’s health
+# and power, make is so that the maximum health value for a player is 200, and
+# the maximum power value is 25) and call the fight method.
+
+class Player
+
+  @name
+  @power
+  @health
+
+  def Alive?
+    if player1 or player2 not alive
+  end
+
+  def damage
+    player1 health -= player2 power
+  end
+
+  def stats
+  end
+
+  def fight
+  end
+
+  loop do
+    start fight
+    finnish is allive true
+    display stats
+  end
+
+end
+
+create two players each with random health and power
