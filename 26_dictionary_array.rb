3ded10788b1b74e71d45212f9e881cce6e7b9066
tracykaur <tracykaur@yahoo.com>
2018-04-26 08:48:35 +1000

    dictionary array challenge

---
+++ 26_dictionary_array.rb
@@ -0,0 +1,28 @@
+# Dictionary
+#
+# You've done this one before. This time, complete the challenge
+# without using a hash. Arrays only.
+#
+# Extend the Dictionary class.
+#
+# It should have three methods:
+# .add_word(word, definition) -> Should store a word and definition
+# .lookup(word) -> Should return a definition
+# .total_words -> Should return the number of words stored
+#
+# Difficulty:
+# 6/10
+#
+# Example:
+# dictionary = Dictionary.new
+# dictionary.add_word('ruby', 'A precious stone')
+# dictionary.total_words -> should return 1
+# dictionary.lookup('ruby') -> should return 'A precious stone'
+#
+# Check your solution by running the tests:
+# ruby tests/26_dictionary_array_test.rb
+#
+
+class Dictionary
+  # Your code here
+end
