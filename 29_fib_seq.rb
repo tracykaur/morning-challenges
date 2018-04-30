32b381d07782782411f353f2f8fc94d3f8aaed4b
Jeff Wong <jeffmjwong@gmail.com>
2018-05-01 09:55:42 +1000

    Solve fib-seq

--- 29_fib_seq.rb
+++ 29_fib_seq.rb
@@ -1,1 +1,9 @@
-# 2. There is an employee at The Company who needs some code written that will provide them with the first 100 numbers of the Fibonacci sequence (no one really knows why the employee needs this information but let's do it anyway!).
+# 2. There is an employee at The Company who needs some code written that will
+# provide them with the first 100 numbers of the Fibonacci sequence (no one
+# really knows why the employee needs this information but let's do it anyway!).
+
+fib_array = [0, 1]
+while fib_array.length < 100
+  fib_array.push(fib_array[(fib_array.length - 1)] + fib_array[(fib_array.length - 2)])
+end
+puts fib_array
