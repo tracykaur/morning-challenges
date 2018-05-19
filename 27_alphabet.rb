08e2d8c96f287cf92b6bf558905499906a631431
Revel Roestorf <revel.roestorf@gmail.com>
2018-05-19 06:57:22 +0200

    solved

--- 27_alphabet.rb
+++ 27_alphabet.rb
@@ -1,3 +1,9 @@
 # Have the function AlphabetSoup(str) take the str string parameter being passed
 # and return the string with the letters in alphabetical order (ie. hello becomes ehllo).
 # Assume numbers and punctuation symbols will not be included in the string.
+
+def alphabetSoup(str)
+  puts str.chars.sort.join
+end
+
+alphabetSoup('hello')
