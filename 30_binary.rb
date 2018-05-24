73bd4eb37e27370e96772d92fe2fc46e903b9bba
Revel Roestorf <revel.roestorf@gmail.com>
2018-05-24 13:23:53 +0200

    solved

--- 30_binary.rb
+++ 30_binary.rb
@@ -1,3 +1,18 @@
 #Write a Ruby function to convert an integer value to binary.
 
 #if you want write a test to validate your method.
+
+def integer_to_binary(x)
+  d = []
+  loop do
+    if x > 0
+      d.push(x % 2)
+      x = x / 2
+    else
+      break
+    end
+  end
+  puts d
+end
+
+integer_to_binary(156)
